022ee46456702f7fe42884eb511cbd8733d15f63
Chris Wilson <chris@chris-wilson.co.uk>
2015-12-14 21:09:30 +0000

    drm/i915: Split early global GTT initialisation
    Initialising the global GTT is tricky as we wish to use the drm_mm range
    manager during the modesetting initialisation (to capture stolen
    allocations from the BIOS) before we actually enable GEM. To overcome
    this, we currently setup the drm_mm first and then carefully rebind
    them.
    
    v2:
    (Matthew Auld)
      - s/init_global_gtt/init_ggtt/ - in favour of consistency
      - rebase -nightly
    
    Signed-off-by: Matthew Auld <matthew.auld@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -1228,8 +1228,6 @@ static void i915_driver_cleanup_mmio(struct drm_i915_private *dev_priv)
 static int i915_driver_init_hw(struct drm_i915_private *dev_priv)
 {
 	struct drm_device *dev = dev_priv->dev;
-	struct i915_ggtt *ggtt = &dev_priv->ggtt;
-	uint32_t aperture_size;
 	int ret;
 
 	if (i915_inject_load_failure())
@@ -1292,19 +1290,6 @@ static int i915_driver_init_hw(struct drm_i915_private *dev_priv)
 		}
 	}
 
-	aperture_size = ggtt->mappable_end;
-
-	ggtt->mappable =
-		io_mapping_create_wc(ggtt->mappable_base,
-				     aperture_size);
-	if (!ggtt->mappable) {
-		ret = -EIO;
-		goto out_ggtt;
-	}
-
-	ggtt->mtrr = arch_phys_wc_add(ggtt->mappable_base,
-					      aperture_size);
-
 	pm_qos_add_request(&dev_priv->pm_qos, PM_QOS_CPU_DMA_LATENCY,
 			   PM_QOS_DEFAULT_VALUE);
 
@@ -1334,7 +1319,6 @@ static int i915_driver_init_hw(struct drm_i915_private *dev_priv)
 
 out_ggtt:
 	i915_ggtt_cleanup_hw(dev);
-
 	return ret;
 }
 
@@ -1345,14 +1329,11 @@ out_ggtt:
 static void i915_driver_cleanup_hw(struct drm_i915_private *dev_priv)
 {
 	struct drm_device *dev = dev_priv->dev;
-	struct i915_ggtt *ggtt = &dev_priv->ggtt;
 
 	if (dev->pdev->msi_enabled)
 		pci_disable_msi(dev->pdev);
 
 	pm_qos_remove_request(&dev_priv->pm_qos);
-	arch_phys_wc_del(ggtt->mtrr);
-	io_mapping_free(ggtt->mappable);
 	i915_ggtt_cleanup_hw(dev);
 }
 
