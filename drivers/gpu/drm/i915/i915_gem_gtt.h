d3e9ed00f72d9aa07a59030ca1839c279068a6cf
Matthew Auld <matthew.auld@intel.com>
2016-12-07 15:30:33 +0000

    drm/i915: add support for 1G ppgtt pages
    Signed-off-by: Matthew Auld <matthew.auld@intel.com>

--- drivers/gpu/drm/i915/i915_gem_gtt.h
+++ drivers/gpu/drm/i915/i915_gem_gtt.h
@@ -108,9 +108,15 @@ typedef uint64_t gen8_ppgtt_pml4e_t;
  * The difference as compared to normal x86 3 level page table is the PDPEs are
  * programmed via register.
  *
- * GEN8 48b legacy style address is defined as a 4 level page table:
- * 47:39 | 38:30 | 29:21 | 20:12 |  11:0
- * PML4E | PDPE  |  PDE  |  PTE  | offset
+ * GEN8 48b legacy style address with 4K is defined as a 4 level
+ * page table:
+ * 47:39 | 38:30 | 29:21 | 20:12 |  11:0  |
+ * PML4E | PDPE  |  PDE  |  PTE  | offset |
+ *
+ * GEN8 48b legacy style address with 1G page is defined as a 2 level page
+ * table:
+ * 47:39 | 38:30 |         29:0           |
+ * PML4E | PDPE  |        offset          |
  */
 #define GEN8_PML4ES_PER_PML4		512
 #define GEN8_PML4E_SHIFT		39
