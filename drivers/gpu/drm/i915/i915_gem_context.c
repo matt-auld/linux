f12e79a55b3c89e6cab38ca40fc991bc35920c7a
Chris Wilson <chris@chris-wilson.co.uk>
2017-01-24 22:51:43 +0000

    drm/i915: Live testing for context execution
    Check we can create and execution within a context.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/i915_gem_context.c
+++ drivers/gpu/drm/i915/i915_gem_context.c
@@ -1172,4 +1172,5 @@ int i915_gem_context_reset_stats_ioctl(struct drm_device *dev,
 
 #if IS_ENABLED(CONFIG_DRM_I915_SELFTEST)
 #include "selftests/mock_context.c"
+#include "selftests/i915_gem_context.c"
 #endif
