f3004582f666c66a2c998091fefd689dac3d0553
Matthew Auld <matthew.auld@intel.com>
2017-01-30 19:14:43 +0000

    drm/i915: pass page_size to insert_entires

--- drivers/gpu/drm/i915/i915_gem_gtt.h
+++ drivers/gpu/drm/i915/i915_gem_gtt.h
@@ -328,6 +328,7 @@ struct i915_address_space {
 	void (*insert_entries)(struct i915_address_space *vm,
 			       struct sg_table *st,
 			       u64 start,
+			       unsigned long page_size,
 			       enum i915_cache_level cache_level,
 			       u32 flags);
 	void (*cleanup)(struct i915_address_space *vm);
