c0cc27df8782cbfce8efdb519afba080dfbe500e
Matthew Auld <matthew.auld@intel.com>
2017-02-16 15:06:49 +0000

    add bug_on

--- drivers/gpu/drm/i915/i915_gem_gtt.c
+++ drivers/gpu/drm/i915/i915_gem_gtt.c
@@ -1036,6 +1036,8 @@ static void gen8_ppgtt_insert_3lvl(struct i915_address_space *vm,
 		.max = iter.dma + iter.sg->length,
 	};
 
+	GEM_BUG_ON(page_size != I915_GTT_PAGE_SIZE);
+
 	gen8_ppgtt_insert_pte_entries(ppgtt, &ppgtt->pdp, &iter,
 				      start, cache_level);
 }
