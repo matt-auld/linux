b24b637193ea4cd2c0aeac8f283e7cbac8694d54
Chris Wilson <chris@chris-wilson.co.uk>
2017-10-02 11:15:05 +0100

    mm,drm/i915: Mark pinned shmemfs pages as unevictable
    Similar in principle to the treatment of get_user_pages, pages that
    i915.ko acquires from shmemfs are not immediately reclaimable and so
    should be excluded from the mm accounting and vmscan until they have
    been returned to the system via shrink_slab/i915_gem_shrink. By moving
    the unreclaimable pages off the inactive anon lru, not only should
    vmscan be improved by avoiding walking unreclaimable pages, but the
    system should also have a better idea of how much memory it can reclaim
    at that moment in time.
    
    Note, however, the interaction with shrink_slab which will move some
    mlocked pages back to the inactive anon lru.
    
    Suggested-by: Dave Hansen <dave.hansen@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Matthew Auld <matthew.auld@intel.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: "Kirill A . Shutemov" <kirill.shutemov@linux.intel.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Michal Hocko <mhocko@suse.com>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -2194,6 +2194,9 @@ void __i915_gem_object_truncate(struct drm_i915_gem_object *obj)
 	obj->mm.pages = ERR_PTR(-EFAULT);
 }
 
+extern void mlock_vma_page(struct page *page);
+extern unsigned int munlock_vma_page(struct page *page);
+
 static void
 i915_gem_object_put_pages_gtt(struct drm_i915_gem_object *obj,
 			      struct sg_table *pages)
@@ -2214,6 +2217,10 @@ i915_gem_object_put_pages_gtt(struct drm_i915_gem_object *obj,
 
 		if (obj->mm.madv == I915_MADV_WILLNEED)
 			mark_page_accessed(page);
+
+		lock_page(page);
+		munlock_vma_page(page);
+		unlock_page(page);
 
 		put_page(page);
 	}
@@ -2420,6 +2427,10 @@ rebuild_st:
 		}
 		last_pfn = page_to_pfn(page);
 
+		lock_page(page);
+		mlock_vma_page(page);
+		unlock_page(page);
+
 		/* Check that the i965g/gm workaround works. */
 		WARN_ON((gfp & __GFP_DMA32) && (last_pfn >= 0x00100000UL));
 	}
@@ -2462,8 +2473,13 @@ rebuild_st:
 err_sg:
 	sg_mark_end(sg);
 err_pages:
-	for_each_sgt_page(page, sgt_iter, st)
+	for_each_sgt_page(page, sgt_iter, st) {
+		lock_page(page);
+		munlock_vma_page(page);
+		unlock_page(page);
+
 		put_page(page);
+	}
 	sg_free_table(st);
 	kfree(st);
 
