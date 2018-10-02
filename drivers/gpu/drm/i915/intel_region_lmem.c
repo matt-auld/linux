0be4501c127054f5a0c9752578f603f1a6a74701
Matthew Auld <matthew.auld@intel.com>
2018-10-02 20:04:43 +0100

    drm/i915/lmem: support pwrite
    We need to add support for pwrite'ing an LMEM object.
    
    Signed-off-by: Matthew Auld <matthew.auld@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Abdiel Janulgue <abdiel.janulgue@linux.intel.com>

--- drivers/gpu/drm/i915/intel_region_lmem.c
+++ drivers/gpu/drm/i915/intel_region_lmem.c
@@ -80,11 +80,84 @@ out_unpin:
 	return ret;
 }
 
+static int region_lmem_pwrite(struct drm_i915_gem_object *obj,
+			      const struct drm_i915_gem_pwrite *args)
+{
+	struct drm_i915_private *i915 = to_i915(obj->base.dev);
+	intel_wakeref_t wakeref;
+	char __user *user_data;
+	unsigned int offset;
+	unsigned long idx;
+	u64 remain;
+	int ret;
+
+	ret = i915_gem_object_wait(obj,
+				   I915_WAIT_INTERRUPTIBLE,
+				   MAX_SCHEDULE_TIMEOUT);
+	if (ret)
+		return ret;
+
+	ret = i915_gem_object_pin_pages(obj);
+	if (ret)
+		return ret;
+
+	ret = mutex_lock_interruptible(&i915->drm.struct_mutex);
+	if (ret)
+		goto out_unpin;
+
+	wakeref = intel_runtime_pm_get(i915);
+
+	ret = i915_gem_object_set_to_wc_domain(obj, true);
+	mutex_unlock(&i915->drm.struct_mutex);
+	if (ret)
+		goto out_put;
+
+	remain = args->size;
+	user_data = u64_to_user_ptr(args->data_ptr);
+	offset = offset_in_page(args->offset);
+	for (idx = args->offset >> PAGE_SHIFT; remain; idx++) {
+		unsigned long unwritten;
+		void __iomem *vaddr;
+		int length;
+
+		length = remain;
+		if (offset + length > PAGE_SIZE)
+			length = PAGE_SIZE - offset;
+
+		vaddr = i915_gem_object_lmem_io_map_page(obj, idx);
+		if (!vaddr) {
+			ret = -ENOMEM;
+			goto out_put;
+		}
+
+		unwritten = copy_from_user((void __force *)vaddr + offset,
+					   user_data,
+					   length);
+		io_mapping_unmap_atomic(vaddr);
+		if (unwritten) {
+			ret = -EFAULT;
+			goto out_put;
+		}
+
+		remain -= length;
+		user_data += length;
+		offset = 0;
+	}
+
+out_put:
+	intel_runtime_pm_put(i915, wakeref);
+out_unpin:
+	i915_gem_object_unpin_pages(obj);
+
+	return ret;
+}
+
 static const struct drm_i915_gem_object_ops region_lmem_obj_ops = {
 	.get_pages = i915_memory_region_get_pages_buddy,
 	.put_pages = i915_memory_region_put_pages_buddy,
 	.release = i915_gem_object_release_memory_region,
 	.pread = region_lmem_pread,
+	.pwrite = region_lmem_pwrite,
 };
 
 static struct drm_i915_gem_object *
