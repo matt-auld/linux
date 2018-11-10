edf3ccd39db6af5c20ee1330a2cce670a61cbd3e
Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
2018-11-09 17:21:22 -0800

    drm/i915: define HAS_MAPPABLE_APERTURE
    The following patches in the series will use it to avoid certain
    operations when aperture is not available in HW.
    
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Matthew Auld <matthew.auld@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -2169,6 +2169,8 @@ IS_SUBPLATFORM(const struct drm_i915_private *i915,
 #define OVERLAY_NEEDS_PHYSICAL(dev_priv) \
 		(INTEL_INFO(dev_priv)->display.overlay_needs_physical)
 
+#define HAS_MAPPABLE_APERTURE(dev_priv) (dev_priv->ggtt.mappable_end > 0)
+
 /* Early gen2 have a totally busted CS tlb and require pinned batches. */
 #define HAS_BROKEN_CS_TLB(dev_priv)	(IS_I830(dev_priv) || IS_I845G(dev_priv))
 
