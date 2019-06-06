8bba26d12f1bd4e43eb6ea82784cf0b5660e9b41
Abdiel Janulgue <abdiel.janulgue@linux.intel.com>
2019-06-05 17:31:30 -0700

    drm/i915: Provide a way to disable PCIe relaxed write ordering
    For performance writes over PCIe may not be strictly ordered by default.
    This provides an option to expose a kernel configuration option to disable
    relaxed ordering and turn on strict ordering instead for debug purposes.
    
    Signed-off-by: Abdiel Janulgue <abdiel.janulgue@linux.intel.com>
    Signed-off-by: Stuart Summers <stuart.summers@intel.com>
    Cc: Matthew Auld <matthew.auld@intel.com>

--- drivers/gpu/drm/i915/intel_memory_region.c
+++ drivers/gpu/drm/i915/intel_memory_region.c
@@ -286,6 +286,18 @@ int intel_memory_regions_hw_probe(struct drm_i915_private *i915)
 {
 	int err, i;
 
+	/* All platforms currently have system memory */
+	GEM_BUG_ON(!HAS_REGION(i915, REGION_SMEM));
+
+	if (IS_DGFX(i915)) {
+		if (IS_ENABLED(CONFIG_DRM_I915_PCIE_STRICT_WRITE_ORDERING))
+			pcie_capability_clear_word(i915->drm.pdev, PCI_EXP_DEVCTL,
+						   PCI_EXP_DEVCTL_RELAX_EN);
+		else
+			pcie_capability_set_word(i915->drm.pdev, PCI_EXP_DEVCTL,
+						 PCI_EXP_DEVCTL_RELAX_EN);
+	}
+
 	for (i = 0; i < ARRAY_SIZE(i915->mm.regions); i++) {
 		struct intel_memory_region *mem = ERR_PTR(-ENODEV);
 		u16 type, instance;
