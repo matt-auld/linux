11d18f7870d34b36899eb7c2784bf8a887ae9557
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2020-09-16 12:19:32 +0200

    drm/i915: Fix workarounds selftest, part 1
    pin_map needs the ww lock, so ensure we pin both before submission.
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Thomas Hellström <thomas.hellstrom@linux.intel.com>

--- drivers/gpu/drm/i915/gem/i915_gem_object.h
+++ drivers/gpu/drm/i915/gem/i915_gem_object.h
@@ -412,6 +412,9 @@ enum i915_map_type {
 void *__must_check i915_gem_object_pin_map(struct drm_i915_gem_object *obj,
 					   enum i915_map_type type);
 
+void *__must_check i915_gem_object_pin_map_unlocked(struct drm_i915_gem_object *obj,
+						    enum i915_map_type type);
+
 void __i915_gem_object_flush_map(struct drm_i915_gem_object *obj,
 				 unsigned long offset,
 				 unsigned long size);
