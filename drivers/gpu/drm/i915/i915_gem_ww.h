4144de4ca963da37d6998361e270fff73337127a
Thomas Hellström <thomas.hellstrom@intel.com>
2020-09-28 20:07:17 +0200

    drm/i915: Introduce a ww transaction helper
    Introduce a for_i915_gem_ww(){} utility to help make the code
    around a ww transaction more readable.
    
    Signed-off-by: Thomas Hellström <thomas.hellstrom@intel.com>

--- drivers/gpu/drm/i915/i915_gem_ww.h
+++ drivers/gpu/drm/i915/i915_gem_ww.h
@@ -19,11 +19,56 @@ struct i915_gem_ww_ctx {
 	struct ww_acquire_ctx ctx;
 	struct list_head obj_list;
 	struct drm_i915_gem_object *contended;
-	bool intr;
+	unsigned short intr;
+	unsigned short loop;
 };
 
 void i915_gem_ww_ctx_init(struct i915_gem_ww_ctx *ctx, bool intr);
 void i915_gem_ww_ctx_fini(struct i915_gem_ww_ctx *ctx);
 int __must_check i915_gem_ww_ctx_backoff(struct i915_gem_ww_ctx *ctx);
 void i915_gem_ww_unlock_single(struct drm_i915_gem_object *obj);
+
+/* Internal function used by for_i915_gem_ww()! Don't use. */
+static inline int __i915_gem_ww_fini(struct i915_gem_ww_ctx *ww, int err)
+{
+	ww->loop = 0;
+	if (err == -EDEADLK) {
+		err = i915_gem_ww_ctx_backoff(ww);
+		if (!err)
+			ww->loop = 1;
+	}
+
+	if (!ww->loop)
+		i915_gem_ww_ctx_fini(ww);
+
+	return err;
+}
+
+/* Internal function used by for_i915_gem_ww()! Don't use. */
+static inline void
+__i915_gem_ww_init(struct i915_gem_ww_ctx *ww, bool intr)
+{
+	i915_gem_ww_ctx_init(ww, intr);
+	ww->loop = 1;
+}
+
+#if 0 /* Kerneldoc for macros? */
+/**
+ * for_i915_gem_ww - Run code as a ww transaction
+ * @_ww: The context representing the transaction.
+ * @_err: Error variable used during transaction that will be assigned
+ * -EDEADLK if locking fails due to transation collistion.
+ * @_intr: Whether to perform transaction locks interruptible.
+ *
+ * Return: After invoking this macro, the transaction error variable may
+ * have been assigned any value during the transaction, but -EDEADLK is
+ * filtered out. In particular, any transaction relaxation may have assigned
+ * it to -EINTR.
+ */
+#endif
+#define for_i915_gem_ww(_ww, _err, _intr)				\
+	GEM_WARN_ON((_err) != 0);					\
+	for (__i915_gem_ww_init(_ww, _intr), _err = 0; (_ww)->loop;	\
+	     _err = __i915_gem_ww_fini(_ww, _err))
+
 #endif
